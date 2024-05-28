34964ad082f774de244d863c2100bfc99ed68408
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-05-28 13:53:20 +0330

    Add lsp_signature plugin

---
+++ lua/custom/plugins/lsp_signature.lua
@@ -0,0 +1,10 @@
+local plugin = {
+  'ray-x/lsp_signature.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  config = function(_, opts)
+    require('lsp_signature').setup(opts)
+  end,
+}
+
+return plugin
