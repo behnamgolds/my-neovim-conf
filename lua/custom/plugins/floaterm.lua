fe94c22dba00e4b09fec2a0b24f2204c46f5acb5
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-12-03 18:37:14 +0330

    edit floaterm plugin

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -6,6 +6,8 @@ return {
 
         vim.g.floaterm_width = 0.8
         vim.g.floaterm_height = 0.8
+        vim.g.floaterm_title = 'Terminal'
+        vim.g.floaterm_titleposition = 'center'
         vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
         vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
     end
