7647d6ed803ca4bb0fe59564078459898c054915
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-12-03 19:02:21 +0330

    edit floaterm plugin

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -6,9 +6,21 @@ return {
 
         vim.g.floaterm_width = 0.8
         vim.g.floaterm_height = 0.8
-        vim.g.floaterm_title = 'Terminal'
+        -- vim.g.floaterm_autohide = 0
+        -- vim.g.floaterm_title = 'Terminal $1/$2'
+         vim.g.floaterm_title = ' Terminal '
         vim.g.floaterm_titleposition = 'center'
         vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
+        
         vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+        
+        -- vim.keymap.set('n', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
+        -- vim.keymap.set('t', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
+        --
+        -- vim.keymap.set('n', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
+        -- vim.keymap.set('t', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
+        --
+        -- vim.keymap.set('n', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
+        -- vim.keymap.set('t', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
     end
 }
