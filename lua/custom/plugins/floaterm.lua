778f3c6965480638b5bd99add778f84e3cd13616
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-12-03 18:26:04 +0330

    edit floaterm plugin

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -3,6 +3,9 @@ return {
     config = function()
         -- vim.g.floaterm_height = 0.4
         -- vim.g.floaterm_wintype = 'split'
+
+        vim.g.floaterm_width = 0.8
+        vim.g.floaterm_height = 0.8
         vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
         vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
     end
