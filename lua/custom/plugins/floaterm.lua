81cf7002759833d17bd184fa02bdfa007be13911
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2024-12-17 16:16:02 +0330

    Silent floaterm command line

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -1,26 +1,27 @@
 return {
-    'voldikss/vim-floaterm',
-    config = function()
-        -- vim.g.floaterm_height = 0.4
-        -- vim.g.floaterm_wintype = 'split'
+  'voldikss/vim-floaterm',
+  config = function()
+    -- vim.g.floaterm_height = 0.4
+    -- vim.g.floaterm_wintype = 'split'
 
-        vim.g.floaterm_width = 0.8
-        vim.g.floaterm_height = 0.8
-        -- vim.g.floaterm_autohide = 0
-        -- vim.g.floaterm_title = 'Terminal $1/$2'
-         vim.g.floaterm_title = ' Terminal '
-        vim.g.floaterm_titleposition = 'center'
-        vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
-        
-        vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
-        
-        -- vim.keymap.set('n', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
-        -- vim.keymap.set('t', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
-        --
-        -- vim.keymap.set('n', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
-        -- vim.keymap.set('t', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
-        --
-        -- vim.keymap.set('n', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
-        -- vim.keymap.set('t', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
-    end
+    vim.g.floaterm_width = 0.8
+    vim.g.floaterm_height = 0.8
+    -- vim.g.floaterm_autohide = 0
+    -- vim.g.floaterm_title = 'Terminal $1/$2'
+    vim.g.floaterm_title = ' Terminal '
+    vim.g.floaterm_titleposition = 'center'
+    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>', { silent = true })
+    vim.keymap.set('i', '<F1>', '<Esc>:FloatermToggle<CR>', { silent = true })
+
+    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>', { silent = true })
+
+    -- vim.keymap.set('n', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
+    -- vim.keymap.set('t', '<F2>', '<C-\\><C-n>:FloatermNew<CR>')
+    --
+    -- vim.keymap.set('n', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
+    -- vim.keymap.set('t', '<F3>', '<C-\\><C-n>:FloatermNext<CR>')
+    --
+    -- vim.keymap.set('n', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
+    -- vim.keymap.set('t', '<F4>', '<C-\\><C-n>:FloatermPrev<CR>')
+  end,
 }
