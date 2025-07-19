fbb14a6fb6a29f223a2912c5784fdbc8bb2a3c70
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2025-07-19 13:30:14 +0330

    add comment to nvim-toggler plugin file

--- lua/custom/plugins/nvim-toggler.lua
+++ lua/custom/plugins/nvim-toggler.lua
@@ -1,5 +1,6 @@
 return {
   -- invert the word under cursor e.g no <-> yes true <-> false
+  -- default key combo:  <leader>i
   'nguyenvukhang/nvim-toggler',
   config = function()
     require('nvim-toggler').setup {
